c0e382fbd3c73847239dd4aff137ee812e7eea88
animepahe4988 <jatin61210@gmail.com>
2024-10-09 15:49:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,6 +27,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Chuttiya")
 # Force sub channel id, if you want enable force sub
 FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1002044619047"))
 FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1002044619047"))
+FORCE_CHANNEL3 = int(os.environ.get("FORCE_CHANNEL2", "-1002044619047"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
