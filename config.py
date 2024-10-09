f46f59064d6a3b22071dd33dc1dac656cf8fd091
animepahe4988 <jatin61210@gmail.com>
2024-10-09 21:44:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/nDg.jpg")
 
 # Add your text according to you
 HELP_TXT = "<b>Hi!\nI am a bot!\n\nMade for @Anime_Bloodline⚡</b>"
-ABOUT_TXT = "<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={6199677027}'>ઝꪋઝꪋઽꫝꪱ</a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/ShadowKakashi>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Anime_Bloodline>Anime Bloodline</a>\n├⋗ Support Group : <a href=https://t.me/community_Weebs>Community Weebs</a>\n╚═════════════════⦿</b>"
+ABOUT_TXT = "<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={6199677027}'>ઝꪋઝꪋઽꫝꪱ</a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/ShadowKakashi>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Anime_Bloodline>Anime Bloodline</a></b>"
 
 # start message
 START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first}\n\nI am Madara Uchiha a file store bot Made for @Anime_Bloodline ⚡</b>")
