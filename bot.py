5170dbc116fc75a8cafb6ea724a96c5adc4facce
animepahe4988 <jatin61210@gmail.com>
2024-10-09 18:49:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,9 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Anime_Bloodline")
         self.LOGGER(__name__).info(f""" \n\n       
-
+░█▀▀▀█ ░█─░█ ░█─▄▀ ░█─░█ ░█▄─░█ ─█▀▀█ 
+─▀▀▀▄▄ ░█─░█ ░█▀▄─ ░█─░█ ░█░█░█ ░█▄▄█ 
+░█▄▄▄█ ─▀▄▄▀ ░█─░█ ─▀▄▄▀ ░█──▀█ ░█─░█
                                           """)
         self.username = usr_bot_me.username
         #web-response
